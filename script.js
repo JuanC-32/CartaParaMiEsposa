0f1c3f3c4a93c4a586b6b91ef34d1c0f621229b9
Juan Camilo Caicedo <juancamilocaicedo3@gmail.com>
2025-11-09 22:14:57 -0500

    Se realizo responsive para otros dispositivos

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  var envelope = $("#envelope");
-  var btn_open = $("#open");
-  var btn_reset = $("#reset");
-
-  envelope.click(function () {
-    open();
-  });
-  btn_open.click(function () {
-    open();
-  });
-  btn_reset.click(function () {
-    close();
-  });
+  const envelope = $("#envelope");
+  const btnOpen = $("#open");
+  const btnReset = $("#reset");
 
   function open() {
     envelope.addClass("open").removeClass("close");
+    envelope.addClass("vibrate");
+    setTimeout(() => envelope.removeClass("vibrate"), 500);
   }
+
   function close() {
     envelope.addClass("close").removeClass("open");
+    envelope.addClass("vibrate");
+    setTimeout(() => envelope.removeClass("vibrate"), 500);
   }
+
+  envelope.click(open);
+  btnOpen.click(open);
+  btnReset.click(close);
 });
+
